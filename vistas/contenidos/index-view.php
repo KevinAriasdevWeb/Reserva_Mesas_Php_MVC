cd80962154eaa8f4badc0f2c796785d530ffeaa7
KevinAriasdevWeb <kev.arias.araya@gmail.com>
2023-10-28 05:23:01 -0300

    Actualizacion de irl imagen 2

--- vistas/contenidos/index-view.php
+++ vistas/contenidos/index-view.php
@@ -15,13 +15,13 @@
         <div class="container__menu">
             <div class="logo_index">
             
-            <img src="<?php echo SERVERURL; ?>/vistas/images/logo3.png" alt="">
+            <img src="<?php echo SERVERURL; ?>vistas/images/logo3.png" alt="">
             </div>
             <div class="menu">
                 <i class="fas fa-bars" id="btn_menu"></i>
                 <div id="back_menu"></div>
                 <nav id="nav">
-                    <img src="/mesas_reservas/vistas/images/logo3.png" alt="">
+                    <img src="<?php echo SERVERURL; ?>vistas/images/logo3.png" alt="">
                     <ul>
 
                         <li><a href="<?php echo SERVERURL; ?>" id="selected">Inicio</a></li>
@@ -41,7 +41,7 @@
             <div class="cover">
                 <div class="text">
                 <h1 style="background-color:#CA9F2D;" >RESTO-BAR PLATITA</h1>
-                    <img src="<?php echo SERVERURL; ?>/mesas_reservas/vistas/images/chef.png" alt="chef">
+                    <img src="<?php echo SERVERURL; ?>vistas/images/chef.png" alt="chef">
                     
 
                     <p>
@@ -56,13 +56,13 @@
                 </div>
 
                 <div class="svg">
-                    <img src="/mesas_reservas/vistas/images/logo_main.png" alt="">
+                    <img src="<?php echo SERVERURL; ?>vistas/images/logo_main.png" alt="">
                 </div>
             </div>
         </div>
     </main>
 
-    <script src="/mesas_reservas/vistas/js/script.js"></script>
+    <script src="<?php echo SERVERURL; ?>vistas/js/script.js"></script>
 
    <!--  footer -->
    <footer class="footer-section">
